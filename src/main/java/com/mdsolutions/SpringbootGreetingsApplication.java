76b802186e7dc9e6319234c2fb38df4611f2d578
gadhilinga <boda.gadhilinga@gmail.com>
2022-05-28 18:54:34 +0530

    Greeting initial Commit

---
+++ src/main/java/com/mdsolutions/SpringbootGreetingsApplication.java
@@ -0,0 +1,13 @@
+package com.mdsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGreetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGreetingsApplication.class, args);
+	}
+
+}
